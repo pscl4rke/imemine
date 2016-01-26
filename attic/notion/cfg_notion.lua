6ca3bb10f5f20be6ef8660e6c3773be6c1c90b5a
P. S. Clarke (ZnLu1404) <pscl4rke@googlemail.com>
2016-01-26 10:28:58 +0000

    Notion: A few WM experiments atticed.

---
+++ attic/notion/cfg_notion.lua
@@ -0,0 +1,21 @@
+
+
+
+-- Copied from /etc/X11/notion/cfg_debian.lua:
+META="Mod1+"
+ALTMETA=""
+
+
+-- Copied from /etc/X11/notion/cfg_default.lua:
+dopath("cfg_notioncore")
+dopath("cfg_kludges")
+dopath("cfg_layouts")
+--
+dopath("mod_query")
+dopath("mod_menu")
+dopath("mod_tiling")
+dopath("mod_statusbar")
+dopath("mod_sp")
+-- Debian extra configuration files
+--dopath("cfg_debian_ext")  -- file is empty
+
