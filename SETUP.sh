d9686ccee9811905c36e3b9d2f0c379cd07564a8
P. S. Clarke (ZnLu1404) <pscl4rke@googlemail.com>
2017-11-30 21:01:26 +0000

    Vim: Vendor in the ledger plugin.

--- SETUP.sh
+++ SETUP.sh
@@ -157,6 +157,7 @@ wantlink "openbox/autostart" ".config/lxsession/Lubuntu/autostart"
 # have both /usr/bin/ack-grep and /usr/bin/ack...
 wantlink "/usr/bin/ack-grep" "bin/ack"
 
+wantlink "vendor/ledger.vim" ".vim/syntax/ledger.vim"
 wantlink "vendor/urxvt_perl_vtwheel" ".urxvt/perl/vtwheel"
 wantlink "/usr/share/vim/addons/syntax" ".vim_addons/syntax"
 
