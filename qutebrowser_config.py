2d2fcec1c1539414528af0686af3c9b78c64bc90
P. S. Clarke (ExUb2004) <pscl4rke@googlemail.com>
2021-02-17 13:22:33 +0000

    Qutebrowser: Use readability in venv

--- qutebrowser_config.py
+++ qutebrowser_config.py
@@ -1,1 +1,14 @@
+
+import sys
+python_executable = sys.prefix + "/bin/python"
+#python_executable = sys.executable
+
+import os
+imemine = os.path.dirname(__file__)
+
 c.tabs.background = True
+
+#config.bind("r", "spawn --userscript /home/psc/imemine/vendor/qutebrowser_readability")
+#config.bind("r", "spawn --userscript /home/psc/.virtualenvs/qutebrowser/bin/python /home/psc/imemine/vendor/qutebrowser_readability")
+config.bind("r", "spawn --userscript %s %s" % (
+    python_executable, os.path.join(imemine, "vendor/qutebrowser_readability")))
