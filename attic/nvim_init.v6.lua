910343f40c419eb61250743f722871580593945c
P. S. Clarke (ZnUb2004) <pscl4rke@googlemail.com>
2024-10-19 13:42:03 +0100

    Nvim: More usage of lua config

--- attic/nvim_init.v6.lua
+++ attic/nvim_init.v6.lua
@@ -129,25 +129,6 @@ require("bufferline").setup {
     }
 }
 
--- Status Bar
-require("lualine").setup {
-    options = {
-        theme = "everforest", -- or "16color"
-        icons_enabled = false,
-        component_separators = "",
-        section_separators = "",
-    },
-    sections = {
-        -- [a|b|c...x|y|z]
-        lualine_a = {"filename", "%m%r%h%w"},
-        lualine_b = {"fileformat", "encoding", "filetype"},
-        lualine_c = {"diagnostics"},
-        lualine_x = {"branch", "diff"},
-        lualine_y = {"%03.3b", "%(%)0x%02.2B"},  -- the %(%) fools lualine into interpretting %
-        lualine_z = {"location", "progress", "%L"},
-    },
-}
-
 -- Quickfix
 function table_contains(tbl, x) -- euugh... surely lua has a built-in?!?!
     found = false
@@ -170,6 +151,7 @@ require("lspconfig").bashls.setup {}
 
 require("myconf.completion")
 require("myconf.colours")
+require("myconf.statusbar")
 
 -- Also to research
 --  makeprg
