ebe56c4d05f8600e37a5396b4769374b9ea25879
P. S. Clarke (PnDb11) <bcebewshfvs2@example.com>
2025-08-01 20:21:27 +0100

    Nvim: Hunk navigation

--- nvim/lua_myconf/gitsigns.lua
+++ nvim/lua_myconf/gitsigns.lua
@@ -4,3 +4,11 @@ require("gitsigns").setup {
     numhl = true,
     linehl = true,
 }
+
+-- Brackets originally jump sentences; turns out I never use them
+--  (use "z." afterwards to scroll to centre of screen, doesn't always work!)
+vim.keymap.set("n", "(", "<cmd>Gitsigns prev_hunk<enter>z.")
+vim.keymap.set("n", ")", "<cmd>Gitsigns next_hunk<enter>z.")
+-- Not understood by older versions of gitsigns...
+--vim.keymap.set("n", "(", "<cmd>Gitsigns nav_hunk prev<enter>")
+--vim.keymap.set("n", ")", "<cmd>Gitsigns nav_hunk next<enter>")
